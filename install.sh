63a9c68f5ca19da263cf8e8263575c2c0718ded2
Shaun Ostoic <ostoic@proton.me>
2025-07-20 00:38:16 -0400

    build: Add dotfiles module parameter to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,40 @@
 #!/usr/bin/env bash
 
+# Exit on first error
+set -e
+
+# Ensure git submodules are pulled and updated.
 git submodule update --init --recursive
+
+
+POSITIONAL_ARGS=()
+
+ALL=0
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -a|--all)
+      ALL=1
+      shift # past argument
+      ;;
+    -*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [ "$ALL" -eq 1 ]; then
+    echo hi
+    MODULES=$(ls ./*/)
+else
+    MODULES="${POSITIONAL_ARGS[*]}"
+fi
+
+echo Installing "$MODULES" for user "$(whoami)"
+
+# Stow the modules.
+stow -t "$HOME" "$MODULES"
